d5a1b71fff25c096766f51f85eb98d114e34467b
Grant Moss <grantmossdev@gmail.com>
2023-03-09 10:47:56 -0700

    got it working with jquery

--- src/index.js
+++ src/index.js
@@ -1,3 +1,175 @@
+import Highcharts from "highcharts";
 import "./main.css";
 
-console.log("starting new project!");
+// console.log("starting new project!");
+
+function chart1(div) {
+  Highcharts.chart(div, {
+    title: {
+      text: "U.S Solar Employment Growth by Job Category, 2010-2020",
+      align: "left",
+    },
+
+    subtitle: {
+      text: 'Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>',
+      align: "left",
+    },
+
+    yAxis: {
+      title: {
+        text: "Number of Employees",
+      },
+    },
+
+    xAxis: {
+      accessibility: {
+        rangeDescription: "Range: 2010 to 2020",
+      },
+    },
+
+    legend: {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "middle",
+    },
+
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false,
+        },
+        pointStart: 2010,
+      },
+    },
+
+    series: [
+      {
+        name: "Installation & Developers",
+        data: [
+          43934, 48656, 65165, 81827, 112143, 142383, 171533, 165174, 155157,
+          161454, 154610,
+        ],
+      },
+      {
+        name: "Manufacturing",
+        data: [
+          24916, 37941, 29742, 29851, 32490, 30282, 38121, 36885, 33726, 34243,
+          31050,
+        ],
+      },
+      {
+        name: "Sales & Distribution",
+        data: [
+          11744, 30000, 16005, 19771, 20185, 24377, 32147, 30912, 29243, 29213,
+          25663,
+        ],
+      },
+      {
+        name: "Operations & Maintenance",
+        data: [
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          11164,
+          11218,
+          10077,
+        ],
+      },
+      {
+        name: "Other",
+        data: [
+          21908, 5548, 8105, 11248, 8989, 11816, 18274, 17300, 13053, 11906,
+          10073,
+        ],
+      },
+    ],
+
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 500,
+          },
+          chartOptions: {
+            legend: {
+              layout: "horizontal",
+              align: "center",
+              verticalAlign: "bottom",
+            },
+          },
+        },
+      ],
+    },
+  });
+}
+
+function chart2(div) {
+  Highcharts.chart(div, {
+    chart: {
+      type: "line",
+    },
+    title: {
+      text: "Monthly Average Temperature",
+    },
+    subtitle: {
+      text:
+        "Source: " +
+        '<a href="https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature" ' +
+        'target="_blank">Wikipedia.com</a>',
+    },
+    xAxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+    },
+    yAxis: {
+      title: {
+        text: "Temperature (°C)",
+      },
+    },
+    plotOptions: {
+      line: {
+        dataLabels: {
+          enabled: true,
+        },
+        enableMouseTracking: false,
+      },
+    },
+    series: [
+      {
+        name: "Reggane",
+        data: [
+          16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8, 38.4, 35.5, 29.2, 22.0,
+          17.8,
+        ],
+      },
+      {
+        name: "Tallinn",
+        data: [
+          -2.9, -3.6, -0.6, 4.8, 10.2, 14.5, 17.6, 16.5, 12.0, 6.5, 2.0, -0.9,
+        ],
+      },
+    ],
+  });
+}
+
+$(document).on("wb-ready.wb", (event) => {
+  chart1("chart1");
+  chart2("chart2");
+});
